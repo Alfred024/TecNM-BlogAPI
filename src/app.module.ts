0d8dba464c302a3bf83b0b643ec91ace20a372c5
Alfredo <21030761@itcelaya.edu.mx>
2024-02-01 22:22:10 -0600

    Project Setup with TypeORM and EnvConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EnvConfig } from './config/env.config';
 import { JoiValidationSchema } from './config/validation.schema';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogModule } from './blog/blog.module';
+import { AuthModule } from './auth/auth.module';
+import { BloggerModule } from './blogger/blogger.module';
 
 @Module({
   imports: [
@@ -20,12 +23,35 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         port: configService.get('db_port'),
         username: configService.get('db_user'),
         password: configService.get('db_user_pwd'),
-        //database: '5432',
         database: configService.get('db_name'),
         autoLoadEntities: true,
         synchronize: true,
       })
-    }),    
+    }),
+
+    // API modules
+    BlogModule, AuthModule, BloggerModule,    
   ],
 })
 export class AppModule {}
+
+// // Config 2
+// ConfigModule.forRoot({
+//   load: [EnvConfig],
+//   validationSchema: JoiValidationSchema,
+// }),
+
+// TypeOrmModule.forRootAsync({
+//   imports: [ConfigModule],
+//   inject: [ConfigService],
+//   useFactory: (configService : ConfigService) =>({
+//     type: 'postgres',
+//     host: configService.get('db_host'),
+//     port: configService.get('db_port'),
+//     username: configService.get('db_user'),
+//     password: configService.get('db_user_pwd'),
+//     database: configService.get('db_name'),
+//     autoLoadEntities: true,
+//     synchronize: true,
+//   })
+// }),
